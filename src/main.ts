3649dd2e4e8d2b9c34f801deb6213658302b650a
Addemark <addemark@gmail.com>
2024-03-20 09:29:02 +0200

    change the port

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,25 @@
-import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
-import { AppModule } from './app.module';
-import multipart from 'fastify-multipart';
-import fastifyCookie from '@fastify/cookie';
+import { NestFactory } from "@nestjs/core";
+import { ValidationPipe } from "@nestjs/common";
+import { AppModule } from "./app.module";
+import multipart from "fastify-multipart";
+import fastifyCookie from "@fastify/cookie";
 import {
   FastifyAdapter,
   NestFastifyApplication,
-} from '@nestjs/platform-fastify';
-import { TransformInterceptor } from 'src/transform.interceptor';
+} from "@nestjs/platform-fastify";
+import { TransformInterceptor } from "src/transform.interceptor";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter(),
+    new FastifyAdapter()
   );
   app.useGlobalInterceptors(new TransformInterceptor());
   app.register(multipart);
   app.useGlobalPipes(new ValidationPipe());
   await app.register(fastifyCookie, {
-    secret: '', // for cookies signature
+    secret: "", // for cookies signature
   });
-  await app.listen(3000);
+  await app.listen(3010);
 }
 bootstrap();
